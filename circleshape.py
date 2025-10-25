7644cff849d85974b80d29dacd36332bf8bf9d96
Nathan <43457298+nathanrydel@users.noreply.github.com>
2025-10-25 20:28:59 +0100

    fix: formatting

--- circleshape.py
+++ circleshape.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
@@ -8,8 +9,8 @@ class CircleShape(pygame.sprite.Sprite):
         else:
             super().__init__()
 
-        self.position= pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0,0)
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
     def draw(self, screen):
