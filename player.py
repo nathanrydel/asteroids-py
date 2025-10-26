43228c37f3fc8edad8daf6328375bfc063e25f85
Nathan <43457298+nathanrydel@users.noreply.github.com>
2025-10-26 18:08:25 +0000

    player shot cooldown and kill mechanic

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class PLayer(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation: float = 0.0
+        self.shoot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -25,12 +26,16 @@ class PLayer(CircleShape):
         self.rotatation += PLAYER_TURN_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = (
             pygame.Vector2(0, 1).rotate(self.rotatation) * PLAYER_SHOOT_SPEED
         )
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
