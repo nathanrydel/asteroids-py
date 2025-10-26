42449a7240173b813108234f02c6801ae77054cd
Nathan <43457298+nathanrydel@users.noreply.github.com>
2025-10-26 18:03:58 +0000

    update the player class to have shot method

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
+from shot import Shot
 from circleshape import CircleShape
 
 
@@ -23,6 +24,12 @@ class PLayer(CircleShape):
     def rotate(self, dt):
         self.rotatation += PLAYER_TURN_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = (
+            pygame.Vector2(0, 1).rotate(self.rotatation) * PLAYER_SHOOT_SPEED
+        )
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -34,6 +41,8 @@ class PLayer(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotatation)
